479f9a774e471b8604c5b735116d5e4b7bfa1a54
Rogerio Bautz <rogeriobautz@gmail.com>
2022-01-12 11:02:55 -0300

    Add media queries | Fix desktop layout

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-const contentBox = document.getElementsByClassName('content-box')
 const label = document.getElementsByClassName('label')
-//console.log(label[1].parentNode.classList)
 
 for (let i = 0; i < label.length; i++) {
   label[i].addEventListener('click', function () {
     for (j = 0; j < label.length; j++) {
-      //to let only one question open at the time
+      //show only one answer at the time
       if (j != i) {
         label[j].parentNode.classList.remove('active')
       }
